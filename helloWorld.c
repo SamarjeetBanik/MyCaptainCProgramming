ee57f4ceefe3dba193abd5654b0a97ff91169b89
SamarjeetBanik <samarjeetbanik@gmail.com>
2021-01-11 10:06:47 +0530

    initial commit

---
+++ helloWorld.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main()
+{
+	printf("Hello...Bonjour...Hola...Namaste...WORLD!!!");
+	
+	return 0;
+}
---
+++ swapTwoNum.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+	int firstNum = 10;
+	int secondNum = 5;
+	
+	printf("Before swapping \n firstNum = %d \n secondNum = %d \n", firstNum, secondNum);
+																	  //10       //5
+	
+	// Swapping
+	firstNum = firstNum + secondNum; // firstNum = 10 + 5 = 15
+	secondNum = firstNum - secondNum; // secondNum = 15 - 5 = 10
+	firstNum = firstNum - secondNum; // firstNum = 15 - 10 = 5
+	
+	printf("\nAfter swapping \n firstNum = %d \n secondNum = %d", firstNum, secondNum);
+																	//5			//10
+}
